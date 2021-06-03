a20fc152b65a2bd6355415ddb3aad323e0d8fae8
YoonMin <rladbsals23@gmail.com>
2021-06-03 22:14:08 +0900

    feat : Remember Me 인증
    [Remember Me 사용 안할경우]
    JSESSIONID 만 사용하게 되며 해당 쿠키가 삭제 될 경우 사이트에 접속을 시도하면
    로그인 페이지로 리다이렉트 되면서 사용자에게 인증을 요청한다.
    [Remember Me 사용 할 경우]
    JSESSIONID, remember-me 쿠키를 같이 사용하게 되며 JSESSIONID쿠키가 삭제되더라도
    remember-me 쿠키가 만료되지 않고 남아있다면 remember-me쿠키에 담겨있는 정보를 이용해서
    username, passwd 정보를 추출하여 사용자계정을 얻고, 얻은 객체를 통해서 다시 인증을 시도한다.

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,11 +1,13 @@
 package io.security.basicsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -21,14 +23,17 @@ import java.io.IOException;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated();
-        http
+                .anyRequest().authenticated()
+        .and()
                 .formLogin()
-                .loginPage("/loginPage")
+                //.loginPage("/loginPage")
                 .defaultSuccessUrl("/")
                 .failureUrl("/login")
                 .usernameParameter("userId")
@@ -66,6 +71,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         response.sendRedirect("/login");
                     }
                 })
-                .deleteCookies("remember-me");
+                .deleteCookies("remember-me")
+        .and()
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService);
     }
 }
